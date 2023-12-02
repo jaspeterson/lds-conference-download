bfa75b92147a940360055b4a3be4340ef56a3282
Joe Peterson <joseph.as.peterson@gmail.com>
2023-12-01 17:14:14 -0700

    begin refactor for changed site format. get all talk links from a given conference. get all talk text from a given talk

--- importTalks.py
+++ importTalks.py
@@ -1,72 +1,70 @@
 #!/usr/bin/env python3
 
-from bs4 import BeautifulSoup
+from site import USER_BASE
+from bs4 import BeautifulSoup, NavigableString
 import requests
 import re
 import os
 import time
 from random import randint
 
-startTime = time.time()
-root = "https://www.lds.org"
-url = "https://www.lds.org/general-conference/conferences?lang=eng"
-headers = {
-    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+STARTING_CONF_YEAR = 1971
+START_TIME = time.time()
+ROOT = "https://churchofjesuschrist.org"
 
-path = os.getcwd() + "/talks/"
-os.mkdir(path)
-
+USER_AGENTS = [
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36',
+    'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.1 Safari/605.1.15',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 13_1) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.1 Safari/605.1.15'
+]
 
 def random_wait():
     time.sleep((randint(1, 5) * 0.1))
 
+def get_random_agent():
+    return USER_AGENTS[randint(0, len(USER_AGENTS)-1)]
 
-def get_talk(link, directory, author, talkName):
-    talkPage = requests.get(link, headers=headers)
-    talkSoup = BeautifulSoup(talkPage.content, 'html.parser')
-    title = get_talk_title(talkSoup)
-    text = get_talk_text(talkSoup, title, author)
-    fileName = directory + "/" + talkName + ".txt"
-    talkFile = open(fileName, "w+")
-    talkFile.write(text)
-    talkFile.close()
+def get_all_talks_from_conf(year, session):
+    url = ROOT + "/study/general-conference/" + year + "/" + session + "?lang=eng"
 
+    conference_page = requests.get(url, headers={
+        'User-Agent': get_random_agent()
+    })
+    conference_soup = BeautifulSoup(conference_page.content, 'html.parser')
 
-def get_talk_title(soup):
-    return soup.find("h1", class_="title").div.string
+    talk_links = []
+    session_list = conference_soup.find("nav", class_="manifest").ul
 
+    for s in session_list.children:
+        if isinstance(s, NavigableString):
+            continue
+        for talk in s.ul.children:
+            if isinstance(talk, NavigableString):
+                continue
+            talk_links.append(talk.a['href'])
 
-def get_talk_text(soup, title, author):
-    totalText = title + "\n" + author + "\n\n"
-    for paragraph in soup.find("div", class_="body-block").find_all("p"):
-        totalText += paragraph.get_text() + "\n\n"
-    return totalText
+    return talk_links
 
+def get_talk(url):
+    talk_page = requests.get(url, headers={
+        'User-Agent': get_random_agent()
+    })
+    talk_soup = BeautifulSoup(talk_page.content, 'html.parser')
 
-def get_all_talks(link, currDir, session):
-    conferencePage = requests.get(link, headers=headers)
-    conferenceSoup = BeautifulSoup(conferencePage.content, 'html.parser')
-    index = 0
-    for element in conferenceSoup.find_all("a", class_="lumen-tile__link"):
-        ref = element['href']
-        author = element.find(
-            "div", class_="lumen-tile__content").string
-        talkName = str(index) + "_" + session
-        random_wait()
-        get_talk(root + ref, currDir, author, talkName)
-        index += 1
+    talk = []
+    body = talk_soup.find("div", class_="body-block")
+    if body == None:
+        return None
+    talk.append(talk_soup.find("h1", id="title1").text)
+    talk.append(talk_soup.find("p", id="author1").text)
 
-
-page = requests.get(url, headers=headers)
-
-soup = BeautifulSoup(page.content, 'html.parser')
-
-for element in soup.find_all("a", class_="year-line__link"):
-    ref = element['href']
-    text = element.string
-    search = re.search(r"\S+\s\S+", text)
-    if search != None:
-        currDirectory = path + search.group() + "/"
-        os.mkdir(currDirectory)
-        print (search.group() + " - ", (time.time() - startTime), "s")
-        get_all_talks(root + ref, currDirectory, search.group())
+    for e in body.find_all(text=True):
+        if e.parent.name == "sup":
+            continue
+        talk.append(e)
+    return talk
+    
